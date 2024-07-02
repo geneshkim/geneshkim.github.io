e59e735aa6f85c31c4771e64b61916fc62e43f46
Gene Kim <gene.sh.kim@gmail.com>
2024-07-01 18:10:04 -0700

    first test with real personal website

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var menuToggle = document.querySelector('.menu-toggle');
-    var navigation = document.querySelector('.navigation');
-
-    menuToggle.addEventListener('click', function () {
-        navigation.style.display = navigation.style.display === 'block' ? 'none' : 'block';
-    });
-});
